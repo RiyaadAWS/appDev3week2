356ff883e470154376e8bea1e8c2ceef476bd29b
Fish84 <craigacarr@gmail.com>
2020-02-19 13:22:00 +0200

    Initial Commit: Basic hello world

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+//1. Setup/create your GIT account on github.com.
+//
+//2. Create a repository on GIT. Write a simple Java application using IntelliJ and upload to your git repository.
+//
+//3. Create two branches of your repository.
+//
+//4. Fork, clone and modify one git projects from your peers.
+//
+//Please note that all submission must be on Blackboard; submit link to your GitHub repository ONLY.
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.print("And 3rd year begins!");
+    }
+
+
+}
